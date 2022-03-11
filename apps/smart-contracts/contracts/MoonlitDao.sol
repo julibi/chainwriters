01c9ab0db6ff0a9193aabc730e6f55a9a961bc62
Juli <julescodes513@gmail.com>
2022-03-11 12:06:09 +0100

    dao contract wip

--- apps/smart-contracts/contracts/MoonlitDao.sol
+++ apps/smart-contracts/contracts/MoonlitDao.sol
@@ -1,18 +1,29 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.9;
 
-contract MoonlitDao {
-    string private greeting;
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
-    constructor(string memory _greeting) {
-        greeting = _greeting;
+contract MoonlitDao is AccessControlEnumerable {
+  string public NAME;
+  string public DESCRIPTION;
+  address public AUTHOR;
+  string public IPFSLINKS;
+  address public EDITOR;
+  constructor(
+      string memory _name,
+      string memory _description,
+      address _author,
+      string memory _ipfsLink,
+      address _editor
+    ) {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        NAME = _name;
+        AUTHOR = _author;
+        MAX_NFTS = _maxNfts;
+        baseUri = _baseUri;
+        mintPrice = 0.001 ether; 
+        MAX_PER_MINT = 5;
+        METADATA_FROZEN = false;
     }
 
-    function greet() public view returns (string memory) {
-        return greeting;
-    }
-
-    function setGreeting(string memory _greeting) public {
-        greeting = _greeting;
-    }
 }
