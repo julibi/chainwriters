15fbac1d17644c82108c61b7af84bafb57e95575
Juli <julescodes513@gmail.com>
2022-03-11 11:28:46 +0100

    adds deployscript

--- apps/smart-contracts/contracts/Greeter.sol
+++ apps/smart-contracts/contracts/Greeter.sol
@@ -1,10 +1,13 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
-contract MoonlitDao {
+import "hardhat/console.sol";
+
+contract Greeter {
     string private greeting;
 
     constructor(string memory _greeting) {
+        console.log("Deploying a Greeter with greeting:", _greeting);
         greeting = _greeting;
     }
 
@@ -13,6 +16,7 @@ contract MoonlitDao {
     }
 
     function setGreeting(string memory _greeting) public {
+        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
         greeting = _greeting;
     }
 }
--- apps/smart-contracts/scripts/deploy.ts
+++ apps/smart-contracts/scripts/deploy.ts
@@ -1,30 +1,57 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+const hre = require("hardhat");
 
-async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+const wait = (seconds:number) => new Promise((resolve, _) => {
+  setTimeout(resolve, seconds * 1000)
+})
 
-  await greeter.deployed();
+async function deployDAO() {
+  await hre.run('compile');
+  
+  const [deployer] = await hre.ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+  console.log(`Network: ${hre.ethers.network}`);
 
-  console.log("Greeter deployed to:", greeter.address);
+  // MoonlitDAO constructor argument
+  const Greeting = "Hello I am DAO";
+
+  // MoonlitNFT constructor argument
+  const name = "testnftschreiberei";
+  const maxNFTs = 10;
+  const author = "0x1e1a88bdEf16b1906b85c31c625baEf8EAabBA98";
+  const baseUri = "baseURI"
+
+  // deploy DAO contract
+  const MoonlitDaoFactory = await hre.ethers.getContractFactory("MoonlitDao");
+  const MoonlitDao = await MoonlitDaoFactory.deploy(Greeting);
+  await MoonlitDao.deployed();
+  console.log(`DAO contract deployed to: ${MoonlitDao.address} at ${hre.ethers.network}`);
+
+   // deploy NFT contract
+   const MoonlitNFTFactory = await hre.ethers.getContractFactory("MoonlitNFT");
+   const MoonlitNFT = await MoonlitNFTFactory.deploy(MoonlitDao.address, name, maxNFTs, author, baseUri);
+   await MoonlitNFT.deployed();
+   console.log(`NFT contract deployed to: ${MoonlitNFT.address} at ${hre.ethers.network}`);
+
+  // wait around for a bit
+  console.log('Waiting for etherscan/polygonscan once more...')
+  await wait(60);
+
+  // verify contract
+  console.log('Verifying...')
+  await Promise.all([hre.run("verify:verify", {
+    address: MoonlitDao.address,
+    constructorArguments: [Greeting]
+  }),
+  hre.run("verify:verify", {
+    address: MoonlitNFT.address,
+    constructorArguments: [MoonlitDao.address, name, maxNFTs, author, baseUri],
+  })
+  ])
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
+deployDAO().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
