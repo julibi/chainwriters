2d6d9bed3c110d6cdd032a8cce1f14f88b54576e
Juli <julescodes513@gmail.com>
2022-03-22 16:24:21 +0100

    sets up data fetching from subgraph

--- apps/ui/pages/index.tsx
+++ apps/ui/pages/index.tsx
@@ -1,12 +1,45 @@
+import { useEffect } from 'react';
 import { useWeb3React } from '@web3-react/core';
+import gql from 'graphql-tag';
+// import { useQuery } from '@apollo/client';
 import { useEagerConnect } from '../hooks/useEagerConnect';
 import WalletConnection from '../components/WalletConnection';
+import client from '../apolloclient';
 import styled from 'styled-components';
 
+// TODO use useQuery instead!
+export const queryDaos = gql`
+  query exampleQuery {
+    daos {
+      id
+      author
+      address
+      createdAt
+      title
+    }
+  }
+`;
 
 export function Index() {
   const {account} = useWeb3React();
   const hasTried = useEagerConnect();
+  const fetchDaos = async () => {
+    const {
+      data: { daos }
+    } = await client.query({
+      query: queryDaos,
+    });
+  
+    if (!daos.length) {
+      return [];
+    }
+    console.log({ daos });
+    return daos;
+  };
+  
+  useEffect(() => {
+    fetchDaos();
+  }, []);
   return (
     <div>
       <span>MAIN PAGE</span>
