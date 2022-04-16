38fa0360d8aed8b49e07bf59b6119ca6eb7250c7
Juli <julescodes513@gmail.com>
2022-04-16 19:34:00 +0200

    wip

--- apps/smart-contracts/hardhat.config.ts
+++ apps/smart-contracts/hardhat.config.ts
@@ -31,6 +31,11 @@ const config: HardhatUserConfig = {
       url: process.env.MUMBAI_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     }
   },
   solidity:  {
