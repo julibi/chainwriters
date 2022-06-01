24ef8373de3f6f0c6b4c9074513060750f299b1f
Juli <julescodes513@gmail.com>
2022-06-01 19:14:39 +0200

    ui(wip): working with alchemy nft api

--- apps/ui/pages/profile.tsx
+++ apps/ui/pages/profile.tsx
@@ -6,17 +6,20 @@ import { RPC_URLS } from '../connectors';
 const Profile = () => {
   const { account, chainId } = useWeb3React();
   console.log(RPC_URLS[chainId]);
-  const alchemyNFTApi = createAlchemyWeb3(RPC_URLS[chainId]);
   
   const test = useCallback(async() => {
+    if (RPC_URLS[chainId] && createAlchemyWeb3) {
+    // @ts-ignore
+    const web3 = createAlchemyWeb3(RPC_URLS[chainId]);
     let nfts;
-    try {
-      nfts = await alchemyNFTApi.alchemy.getNfts({owner: account})
-    } catch(e: unknown) {
-      console.log({ e });
+      try {
+        nfts = await web3.alchemy.getNfts({owner: account})
+      } catch(e: unknown) {
+        console.log({ e });
+      }
+      console.log({ nfts });
     }
-    console.log({ nfts });
-  }, [account, alchemyNFTApi]);
+  }, [account, chainId]);
 
   useEffect(() => {
     test();
