478ffec531e6b34454f4b2826715a99d5fd5abdc
Juli <julescodes513@gmail.com>
2022-06-13 17:52:16 +0200

    ui(feat): displays projects on profile page

--- apps/ui/hooks/useAllNftsOfUser.ts
+++ apps/ui/hooks/useAllNftsOfUser.ts
@@ -12,39 +12,43 @@ import { FACTORY as FACTORY_ADDRESS } from '../../constants';
 const useAllNftsOfUser = () => {
   const { account, chainId } = useWeb3React();
   const FactoryContract = useFactoryContract();
-  
+  const [loading, setLoading] = useState(false);
   const [allNftsOfUser, setAllNftsOfUser] = useState(null);
   const [allProjectAddresses, setAllProjectAddresses] = useState<string[] | null>(null);
 
   const fetchAllProjectAddresses = useCallback(async() => {
+    setLoading(true);
     if (FactoryContract) {
-      const daosTotal = Number(await FactoryContract.projectDaosLength());
-      // TODO chainId - error handling
-      const provider = new JsonRpcProvider(
-        RPC_URLS[chainId],
-        chainId
-      );
-      const multicall = new Multicall({
-        ethersProvider: provider,
-        tryAggregate: false
-      });
-      const callsArray = [...Array(daosTotal).keys()].map((daoId) => {
-        return {
-          reference: 'projectDaos',
-          methodName: 'projectDaos',
-          methodParameters: [daoId],
+      try {
+        const daosTotal = Number(await FactoryContract.projectDaosLength());
+        const provider = new JsonRpcProvider(
+          RPC_URLS[chainId],
+          chainId
+        );
+        const multicall = new Multicall({
+          ethersProvider: provider,
+          tryAggregate: false
+        });
+        const callsArray = [...Array(daosTotal).keys()].map((daoId) => {
+          return {
+            reference: 'projectDaos',
+            methodName: 'projectDaos',
+            methodParameters: [daoId],
+          };
+        });
+        const myMulticall = {
+          reference: "ALL_DAO_ADDRESSES",
+          contractAddress: FACTORY_ADDRESS,
+          abi: ABI,
+          calls: callsArray
         };
-      });
-      const myMulticall = {
-        reference: "ALL_DAO_ADDRESSES",
-        contractAddress: FACTORY_ADDRESS,
-        abi: ABI,
-        calls: callsArray
-      };
-      // @ts-ignore
-      const multicallResult = await multicall.call(myMulticall);
-      const projectAddresses = multicallResult.results.ALL_DAO_ADDRESSES.callsReturnContext.map(item => item.returnValues[0].toLowerCase());
-      setAllProjectAddresses(projectAddresses);
+        // @ts-ignore
+        const multicallResult = await multicall.call(myMulticall);
+        const projectAddresses = multicallResult.results.ALL_DAO_ADDRESSES.callsReturnContext.map(item => item.returnValues[0].toLowerCase());
+        setAllProjectAddresses(projectAddresses);
+      } catch(e: unknown) {
+        setAllProjectAddresses([]);
+      }
     } 
   }, [FactoryContract, chainId]);
   
@@ -60,6 +64,7 @@ const useAllNftsOfUser = () => {
     }
     const pepoNfts = nfts.ownedNfts.filter(nft => allProjectAddresses.includes(nft.contract.address.toLowerCase()));
     setAllNftsOfUser(pepoNfts);
+    setLoading(false);
     }
   }, [account, chainId, allProjectAddresses]);
 
@@ -75,7 +80,7 @@ const useAllNftsOfUser = () => {
     }
   }, [account, chainId, allProjectAddresses, fetchFromAlchemyAPI]);
   
-  return { allNftsOfUser, allProjectAddresses };
+  return { allNftsOfUser, allProjectAddresses, loading };
 }
 
 export default useAllNftsOfUser;
--- apps/ui/pages/profile.tsx
+++ apps/ui/pages/profile.tsx
@@ -1,15 +1,182 @@
 import React, { useCallback, useEffect } from 'react'
 import { useWeb3React } from '@web3-react/core'
-import useAllNftsOfUser from '../hooks/useAllNftsOfUser';
+import { useRouter } from 'next/router'
+import styled from 'styled-components'
+import useAllNftsOfUser from '../hooks/useAllNftsOfUser'
+import { SectionTitle, SectionTitleWrapper } from '../components/HomePage/ProjectSection';
+import { BaseButton, BASE_BORDER_RADIUS, BASE_BOX_SHADOW, PINK } from '../themes';
+import Loading from '../components/Loading'
+import { useFetchContributedDaos, useFetchCreatedDaos } from '../state/mybookshelf/hooks'
+
+const Root = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 3rem;
+`;
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 3rem;
+
+  @media (max-width: 900px) {
+    padding: 0;
+  }
+`;
+
+const SubHeader = styled.h3`
+  display: inline-block;
+  font-family: 'Roboto Mono Bold';
+  text-align: center;
+  text-transform: uppercase;
+  border-radius: ${BASE_BORDER_RADIUS};
+  box-shadow: ${BASE_BOX_SHADOW};
+  margin-block-end: 3rem;
+  padding: 0.5rem;
+  width: fit-content;
+`;
+
+const Item = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: ${BASE_BORDER_RADIUS};
+  box-shadow: ${BASE_BOX_SHADOW};
+  padding: 1rem;
+  margin-block-end: 1rem;
+`;
+
+const BlockSpan = styled.span`
+  display: inline-block;
+`;
+
+const Title = styled(BlockSpan)`
+  width: 30%;
+`;
+
+const Edition = styled(BlockSpan)`
+  @media (max-width: 900px) {
+    display: none;
+  }
+`;
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+`;
+
+const DetailsButton = styled(BaseButton)`
+  margin-inline-start: 1rem;
+`;
+
+const ReadButton = styled(BaseButton)`
+  margin-inline-start: 1rem;
+  background-color: ${PINK};
+
+  @media (max-width: 900px) {
+    display: none;
+  }
+`;
 
 const Profile = () => {
   const { account, chainId } = useWeb3React();
-  const { allNftsOfUser } = useAllNftsOfUser();
+  const { allNftsOfUser, loading } = useAllNftsOfUser();
+  const router = useRouter();
+  const { data: created, loading: createdLoading, error: createdError } = useFetchCreatedDaos();
+  const { data: contributed, loading: contributedLoading, error: contributedError } = useFetchContributedDaos();
 
-  console.log({ allNftsOfUser });
+  const handleClickRead = useCallback((e, address) => {
+    e.preventDefault();
+    router.push(`/projects/${address}/read`)
+  }, [router]);
+
+  const handleClickDetails = useCallback((e, address) => {
+    e.preventDefault();
+    router.push(`/projects/${address}`)
+  }, [router]);
+
   return (
-    <div>Profile</div>
-  )
+    <Root>
+      <SectionTitleWrapper>
+        <SectionTitle>My Bookshelf</SectionTitle>
+      </SectionTitleWrapper>
+      {loading && <Loading height={200} />}
+      {!loading && !createdLoading && !createdError && (created.daos.length > 0) && (
+        <Section>
+          <SubHeader>My Projects</SubHeader>
+          {created.daos.map((project, idx) => (
+            <Item key={idx}>
+              <Title>{project.title}</Title>
+              <ButtonsWrapper>
+                <DetailsButton
+                  onClick={(e) => handleClickDetails(e, project.address)}
+                >
+                  View Details
+                </DetailsButton>
+                <ReadButton
+                  onClick={(e) => handleClickRead(e, project.address)}
+                >
+                  Read
+                </ReadButton>
+              </ButtonsWrapper>
+            </Item>
+          ))}
+        </Section>
+      )}
+      {!loading && !contributedLoading && !contributedError && (contributed.contributions.length > 0) && (
+        <Section>
+          <SubHeader>My Contributions</SubHeader>
+          {contributed.contributions.map((project, idx) => (
+            <Item key={idx}>
+              <Title>{project.dao.title}</Title>
+              <BlockSpan>{project.role}</BlockSpan>
+              <BlockSpan>{`${project.share} %`}</BlockSpan>
+              <ButtonsWrapper>
+                <DetailsButton
+                  onClick={(e) => handleClickDetails(e, project.address)}
+                >
+                  View Details
+                </DetailsButton>
+              </ButtonsWrapper>
+            </Item>
+          ))}
+        </Section>
+      )}
+      {allNftsOfUser && !loading && (
+        <Section>
+          <SubHeader>My NFTs</SubHeader>
+          {allNftsOfUser.length < 1 && (
+            <BlockSpan>You do not own any Literature NFTs.</BlockSpan>
+          )}
+          {allNftsOfUser.length > 0 &&
+            allNftsOfUser.map((nft, idx) => (
+              <Item key={idx}>
+                <Title>{nft.title}</Title>
+                <Edition>
+                  {Number(nft.id.tokenId) === 1
+                    ? 'Genesis Edition'
+                    : `Edition ${Number(nft.id.tokenId)}`}
+                  {` x ${nft.balance}`}
+                </Edition>
+                <ButtonsWrapper>
+                  <DetailsButton
+                    onClick={(e) => handleClickDetails(e, nft.contract.address)}
+                  >
+                    View Details
+                  </DetailsButton>
+                  <ReadButton
+                    onClick={(e) => handleClickRead(e, nft.contract.address)}
+                  >
+                    Read
+                  </ReadButton>
+                </ButtonsWrapper>
+              </Item>
+            ))}
+        </Section>
+      )}
+    </Root>
+  );
 }
 
 export default Profile
