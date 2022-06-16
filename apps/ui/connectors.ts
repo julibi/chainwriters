9ce467d25294865e7899de88129780344ab4c490
Juli <julescodes513@gmail.com>
2022-06-16 08:48:58 +0200

    testcommit for debugging no 4...

--- apps/ui/connectors.ts
+++ apps/ui/connectors.ts
@@ -2,10 +2,10 @@ import { InjectedConnector } from '@web3-react/injected-connector';
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
 
 export const RPC_URLS: { [chainId: number]: string } = {
-  1: process.env.RPC_URL_ETH_MAINNET,
-  42: process.env.RPC_URL_ETH_RINKEBY,
-  137: process.env.RPC_URL_POLYGON_MAINNET,
-  80001: process.env.RPC_URL_POLYGON_MUMBAI
+  1: process.env.NEXT_URL_RPC_URL_ETH_MAINNET,
+  42: process.env.NEXT_URL_RPC_URL_ETH_RINKEBY,
+  137: process.env.NEXT_URL_RPC_URL_POLYGON_MAINNET,
+  80001: process.env.NEXT_URL_RPC_URL_POLYGON_MUMBAI
 };
 
 export const supportedChainIds = [137, 80001];
