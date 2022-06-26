e8b03b394bd912c16f9bbc331c344823feaa2218
Juli <julescodes513@gmail.com>
2022-06-26 02:52:09 +0200

    build: fixes prop inisde StartAnimation comp

--- apps/ui/components/StartAnimation.tsx
+++ apps/ui/components/StartAnimation.tsx
@@ -4,7 +4,6 @@ import TypeWriter from './TypeWriter';
 const StartAnimation = () => {
   return (
     <TypeWriter
-      fontSize={54}
       text={
         'Start a literature movement on the blockchain now! Create and collect NFTs.'
       }
