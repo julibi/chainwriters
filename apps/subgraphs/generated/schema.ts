a2a88de5d80e07d2093e60bb219fdfc622d6462b
julibi <julescodes513@gmail.com>
2022-08-25 00:16:19 +0200

    chore: (graphs) wip

--- apps/subgraphs/generated/schema.ts
+++ apps/subgraphs/generated/schema.ts
@@ -11,181 +11,35 @@ import {
   BigDecimal
 } from "@graphprotocol/graph-ts";
 
-export class Contribution extends Entity {
+export class Project extends Entity {
   constructor(id: string) {
     super();
     this.set("id", Value.fromString(id));
 
-    this.set("address", Value.fromBytes(Bytes.empty()));
-    this.set("share", Value.fromBigInt(BigInt.zero()));
-    this.set("dao", Value.fromString(""));
-  }
-
-  save(): void {
-    let id = this.get("id");
-    assert(id != null, "Cannot save Contribution entity without an ID");
-    if (id) {
-      assert(
-        id.kind == ValueKind.STRING,
-        "Cannot save Contribution entity with non-string ID. " +
-          'Considering using .toHex() to convert the "id" to a string.'
-      );
-      store.set("Contribution", id.toString(), this);
-    }
-  }
-
-  static load(id: string): Contribution | null {
-    return changetype<Contribution | null>(store.get("Contribution", id));
-  }
-
-  get id(): string {
-    let value = this.get("id");
-    return value!.toString();
-  }
-
-  set id(value: string) {
-    this.set("id", Value.fromString(value));
-  }
-
-  get address(): Bytes {
-    let value = this.get("address");
-    return value!.toBytes();
-  }
-
-  set address(value: Bytes) {
-    this.set("address", Value.fromBytes(value));
-  }
-
-  get share(): BigInt {
-    let value = this.get("share");
-    return value!.toBigInt();
-  }
-
-  set share(value: BigInt) {
-    this.set("share", Value.fromBigInt(value));
-  }
-
-  get role(): string | null {
-    let value = this.get("role");
-    if (!value || value.kind == ValueKind.NULL) {
-      return null;
-    } else {
-      return value.toString();
-    }
-  }
-
-  set role(value: string | null) {
-    if (!value) {
-      this.unset("role");
-    } else {
-      this.set("role", Value.fromString(<string>value));
-    }
-  }
-
-  get dao(): string {
-    let value = this.get("dao");
-    return value!.toString();
-  }
-
-  set dao(value: string) {
-    this.set("dao", Value.fromString(value));
-  }
-}
-
-export class Edition extends Entity {
-  constructor(id: string) {
-    super();
-    this.set("id", Value.fromString(id));
-
-    this.set("maxSupply", Value.fromBigInt(BigInt.zero()));
-    this.set("mintPrice", Value.fromBigInt(BigInt.zero()));
-    this.set("dao", Value.fromString(""));
-  }
-
-  save(): void {
-    let id = this.get("id");
-    assert(id != null, "Cannot save Edition entity without an ID");
-    if (id) {
-      assert(
-        id.kind == ValueKind.STRING,
-        "Cannot save Edition entity with non-string ID. " +
-          'Considering using .toHex() to convert the "id" to a string.'
-      );
-      store.set("Edition", id.toString(), this);
-    }
-  }
-
-  static load(id: string): Edition | null {
-    return changetype<Edition | null>(store.get("Edition", id));
-  }
-
-  get id(): string {
-    let value = this.get("id");
-    return value!.toString();
-  }
-
-  set id(value: string) {
-    this.set("id", Value.fromString(value));
-  }
-
-  get maxSupply(): BigInt {
-    let value = this.get("maxSupply");
-    return value!.toBigInt();
-  }
-
-  set maxSupply(value: BigInt) {
-    this.set("maxSupply", Value.fromBigInt(value));
-  }
-
-  get mintPrice(): BigInt {
-    let value = this.get("mintPrice");
-    return value!.toBigInt();
-  }
-
-  set mintPrice(value: BigInt) {
-    this.set("mintPrice", Value.fromBigInt(value));
-  }
-
-  get dao(): string {
-    let value = this.get("dao");
-    return value!.toString();
-  }
-
-  set dao(value: string) {
-    this.set("dao", Value.fromString(value));
-  }
-}
-
-export class Dao extends Entity {
-  constructor(id: string) {
-    super();
-    this.set("id", Value.fromString(id));
-
-    this.set("author", Value.fromBytes(Bytes.empty()));
-    this.set("address", Value.fromBytes(Bytes.empty()));
+    this.set("creator", Value.fromBytes(Bytes.empty()));
+    this.set("royaltiesSplitter", Value.fromBytes(Bytes.empty()));
     this.set("createdAt", Value.fromBigInt(BigInt.zero()));
     this.set("title", Value.fromString(""));
     this.set("textIpfsHash", Value.fromString(""));
     this.set("auctionsStarted", Value.fromBoolean(false));
     this.set("auctionsEnded", Value.fromBoolean(false));
-    this.set("paused", Value.fromBoolean(false));
   }
 
   save(): void {
     let id = this.get("id");
-    assert(id != null, "Cannot save Dao entity without an ID");
+    assert(id != null, "Cannot save Project entity without an ID");
     if (id) {
       assert(
         id.kind == ValueKind.STRING,
-        "Cannot save Dao entity with non-string ID. " +
+        "Cannot save Project entity with non-string ID. " +
           'Considering using .toHex() to convert the "id" to a string.'
       );
-      store.set("Dao", id.toString(), this);
+      store.set("Project", id.toString(), this);
     }
   }
 
-  static load(id: string): Dao | null {
-    return changetype<Dao | null>(store.get("Dao", id));
+  static load(id: string): Project | null {
+    return changetype<Project | null>(store.get("Project", id));
   }
 
   get id(): string {
@@ -197,22 +51,22 @@ export class Dao extends Entity {
     this.set("id", Value.fromString(value));
   }
 
-  get author(): Bytes {
-    let value = this.get("author");
+  get creator(): Bytes {
+    let value = this.get("creator");
     return value!.toBytes();
   }
 
-  set author(value: Bytes) {
-    this.set("author", Value.fromBytes(value));
+  set creator(value: Bytes) {
+    this.set("creator", Value.fromBytes(value));
   }
 
-  get address(): Bytes {
-    let value = this.get("address");
+  get royaltiesSplitter(): Bytes {
+    let value = this.get("royaltiesSplitter");
     return value!.toBytes();
   }
 
-  set address(value: Bytes) {
-    this.set("address", Value.fromBytes(value));
+  set royaltiesSplitter(value: Bytes) {
+    this.set("royaltiesSplitter", Value.fromBytes(value));
   }
 
   get createdAt(): BigInt {
@@ -276,6 +130,23 @@ export class Dao extends Entity {
     }
   }
 
+  get originalLanguage(): string | null {
+    let value = this.get("originalLanguage");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toString();
+    }
+  }
+
+  set originalLanguage(value: string | null) {
+    if (!value) {
+      this.unset("originalLanguage");
+    } else {
+      this.set("originalLanguage", Value.fromString(<string>value));
+    }
+  }
+
   get subtitle(): string | null {
     let value = this.get("subtitle");
     if (!value || value.kind == ValueKind.NULL) {
@@ -310,40 +181,6 @@ export class Dao extends Entity {
     }
   }
 
-  get metadataCID(): string | null {
-    let value = this.get("metadataCID");
-    if (!value || value.kind == ValueKind.NULL) {
-      return null;
-    } else {
-      return value.toString();
-    }
-  }
-
-  set metadataCID(value: string | null) {
-    if (!value) {
-      this.unset("metadataCID");
-    } else {
-      this.set("metadataCID", Value.fromString(<string>value));
-    }
-  }
-
-  get editions(): Array<string> | null {
-    let value = this.get("editions");
-    if (!value || value.kind == ValueKind.NULL) {
-      return null;
-    } else {
-      return value.toStringArray();
-    }
-  }
-
-  set editions(value: Array<string> | null) {
-    if (!value) {
-      this.unset("editions");
-    } else {
-      this.set("editions", Value.fromStringArray(<Array<string>>value));
-    }
-  }
-
   get auctionsStarted(): boolean {
     let value = this.get("auctionsStarted");
     return value!.toBoolean();
@@ -360,31 +197,5 @@ export class Dao extends Entity {
 
   set auctionsEnded(value: boolean) {
     this.set("auctionsEnded", Value.fromBoolean(value));
-  }
-
-  get paused(): boolean {
-    let value = this.get("paused");
-    return value!.toBoolean();
-  }
-
-  set paused(value: boolean) {
-    this.set("paused", Value.fromBoolean(value));
-  }
-
-  get contributions(): Array<string> | null {
-    let value = this.get("contributions");
-    if (!value || value.kind == ValueKind.NULL) {
-      return null;
-    } else {
-      return value.toStringArray();
-    }
-  }
-
-  set contributions(value: Array<string> | null) {
-    if (!value) {
-      this.unset("contributions");
-    } else {
-      this.set("contributions", Value.fromStringArray(<Array<string>>value));
-    }
   }
 }
--- apps/subgraphs/src/mapping.ts
+++ apps/subgraphs/src/mapping.ts
@@ -1,122 +1,60 @@
-import { BigInt } from '@graphprotocol/graph-ts'
-import { DaoCreated } from "../generated/ProjectFactory/ProjectFactory"
+import { log } from '@graphprotocol/graph-ts';
+import { BigInt } from '@graphprotocol/graph-ts';
 import {
-  AuctionsEnded,
-  AuctionsStarted,
-  AuthorMinted,
-  ContributorAdded,
-  Configured,
-  ExpirationSet,
-  Minted,
-  NextEditionEnabled,
-  TextSet,
-  URISet
-} from '../generated/templates/ProjectDao/ProjectDao';
-import { Contribution, Dao, Edition } from "../generated/schema"
-import { ProjectDao as ProjectDaoTemplate } from '../generated/templates'
-import { log } from "@graphprotocol/graph-ts"
+  AdminChanged,
+  Paused,
+  ProjectCreated,
+  Received,
+  RoleAdminChanged,
+  RoleGranted,
+  RoleRevoked,
+  Unpaused,
+  Upgraded,
+} from '../generated/MoonpageFactory/MoonpageFactory';
+import { Project } from '../generated/schema';
 
-export function handleDaoCreated(event: DaoCreated): void {
-  let dao = new Dao(event.params.dao.toHexString())
-  ProjectDaoTemplate.create(event.params.dao)
-  dao.author = event.params.caller
-  dao.address = event.params.dao
-  dao.title = event.params.title
-  dao.textIpfsHash = event.params.textIpfsHash
-  dao.createdAt = event.block.timestamp
-  dao.auctionsStarted = false
-  dao.auctionsEnded = false
-  dao.save()
-
-  let edition = new Edition(event.params.dao.toHexString() + '1') 
-  edition.dao = dao.id
-  edition.maxSupply = event.params.firstEditionAmount
-  edition.mintPrice = event.params.initialMintPrice
-  edition.save()
+export function handleAdminChanged(event: AdminChanged): void {
+  // // Entities can be loaded from the store using a string ID; this ID
+  // // needs to be unique across all entities of the same type
+  // let entity = ExampleEntity.load(event.transaction.from.toHex());
+  // // Entities only exist after they have been saved to the store;
+  // // `null` checks allow to create entities on demand
+  // if (!entity) {
+  //   entity = new ExampleEntity(event.transaction.from.toHex());
+  //   // Entity fields can be set using simple assignments
+  //   entity.count = BigInt.fromI32(0);
+  // }
+  // // BigInt and BigDecimal math are supported
+  // entity.count = entity.count + BigInt.fromI32(1);
+  // // Entity fields can be set based on event parameters
+  // entity.previousAdmin = event.params.previousAdmin;
+  // entity.newAdmin = event.params.newAdmin;
+  // // Entities can be written to the store with `.save()`
+  // entity.save();
 }
 
-export function handleTextSet(event: TextSet): void {
-  let dao = Dao.load(event.address.toHexString())
-  if (!dao) return
-  dao.textIpfsHash = event.params.textHash
-  dao.save()
+export function handlePaused(event: Paused): void {}
+
+export function handleProjectCreated(event: ProjectCreated): void {
+  let project = new Project(event.params.projectId.toHexString());
+  project.creator = event.params.creator;
+  project.royaltiesSplitter = event.params.royaltiesSplitter;
+  project.createdAt = event.block.timestamp;
+  project.title = event.params.title;
+  project.textIpfsHash = event.params.textIpfsHash;
+  project.auctionsStarted = false;
+  project.auctionsEnded = false;
+  project.save();
 }
 
-export function handleConfigured(event: Configured): void {
-  let dao = Dao.load(event.address.toHexString())
-  if (!dao) return
-  dao.imgIpfsHash = event.params.imgHash
-  dao.blurbIpfsHash = event.params.blurbHash
-  dao.genre = event.params.newGenre
-  dao.subtitle = event.params.newSubtitle
-  dao.save()
-}
+export function handleReceived(event: Received): void {}
 
-export function handleAuctionsStarted(event: AuctionsStarted): void {
-  let dao = Dao.load(event.address.toHexString())
-  if (!dao) return
-  dao.auctionsStarted = true
-  dao.save()
-}
+export function handleRoleAdminChanged(event: RoleAdminChanged): void {}
 
-export function handleAuctionsEnded(event: AuctionsEnded): void {
-  let dao = Dao.load(event.address.toHexString())
-  if (!dao) return
-  dao.auctionsEnded = true
-  dao.save()
-}
+export function handleRoleGranted(event: RoleGranted): void {}
 
-export function handleContributorAdded(event: ContributorAdded): void {
-  let dao = Dao.load(event.address.toHexString())
-  if (!dao) return
-  let contribution = new Contribution(event.params.contributor.toHex() + "-" + event.transaction.hash.toHex() + "-" + event.logIndex.toString())
-  contribution.address = event.params.contributor
-  contribution.share = event.params.share
-  contribution.role = event.params.role
-  contribution.dao = dao.id
-  contribution.save()
-}
+export function handleRoleRevoked(event: RoleRevoked): void {}
 
-export function handleAuthorMinted(event: AuthorMinted): void {
-  let id = event.address.toHexString() + '1'
-  let edition = Edition.load(id) 
-  log.info("handleAuthorMinted", [event.address.toHexString()])
-  if (!edition) return
-  log.info("there is an edition! handleAuthorMing", [edition.id])
-  // NOT WORKING
-}
+export function handleUnpaused(event: Unpaused): void {}
 
-export function handleMinted(event: Minted): void {
-  let edition = Edition.load(event.address.toHexString() + event.params.edition.toHexString())
-  if (!edition) return
-  // NOT WORKING
-  // edition.totalSupply = edition.totalSupply.plus(event.params.amount)
-  // edition.save()
-}
-
-export function handleExpirationSet(event: ExpirationSet): void {
-  let edition = Edition.load(event.address.toHexString() + event.params.edition.toHexString())
-  if (!edition) return
-   // NOT WORKING
-  // edition.expiresAt = event.params.expirationTime
-  // edition.save()
-}
-
-export function handleURISet(event: URISet): void {
-  let dao = Dao.load(event.address.toHexString())
-  if (!dao) return
-  dao.metadataCID = event.params.uri
-  dao.save()
-}
-
-export function handleNextEditionEnabled(event: NextEditionEnabled): void {
-  let edition = new Edition(event.address.toHexString() + event.params.nextEdId.toHexString())
-  let dao = Dao.load(event.address.toHexString())
-  if (!dao) return
-  edition.dao = dao.id
-  edition.maxSupply = event.params.maxSupply
-  edition.mintPrice = event.params.mintPrice
-  edition.save()
-}
-
-// TODO paused && unpaused
+export function handleUpgraded(event: Upgraded): void {}
