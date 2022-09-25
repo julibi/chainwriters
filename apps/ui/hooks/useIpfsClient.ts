2421bfaefba1280fa89cc95199c55cb37c3ee65d
juli <juli@polychainmonsters.com>
2022-09-25 15:14:34 +0200

    fix: ipfs https client issue when uploading

--- apps/ui/hooks/useIpfsClient.ts
+++ apps/ui/hooks/useIpfsClient.ts
@@ -4,15 +4,16 @@ export const useIpfsClient = () => {
   const projectId = process.env.NX_PUBLIC_IPFS_INFURA_PROJECT_ID;
   const projectSecret = process.env.NX_PUBLIC_IPFS_INFURA_SECRET;
   const auth =
-  'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
+    'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
   const client = create({
     host: 'ipfs.infura.io',
     port: 5001,
     protocol: 'https',
+    apiPath: '/api/v0',
     headers: {
       authorization: auth,
-    }
+    },
   });
 
   return client;
-}
+};
