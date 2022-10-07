9e5b4434bfdf2a453f065c71dd534963fb39936c
juli <juli@polychainmonsters.com>
2022-10-07 21:06:45 +0200

    shit

--- apps/ui/apolloclient.ts
+++ apps/ui/apolloclient.ts
@@ -4,7 +4,7 @@ const client = new ApolloClient({
   uri:
     process.env.NEXT_PUBLIC_ENVIRONMENT === 'DEV'
       ? 'https://api.thegraph.com/subgraphs/name/julibi/moonpage-graphs-dev'
-      : 'https://api.thegraph.com/subgraphs/name/julibi/moonpage-graphs',
+      : 'https://api.thegraph.com/subgraphs/name/julibi/moonpage-graphs-prod',
   cache: new InMemoryCache(),
 });
 
