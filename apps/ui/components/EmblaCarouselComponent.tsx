f0aee6c9848d3286ad968e7f5f6d0b38355f7254
juli <juli@polychainmonsters.com>
2022-11-20 15:49:53 +0100

    feat(ui): use embla as carousel wip

---
+++ apps/ui/components/EmblaCarouselComponent.tsx
@@ -0,0 +1,126 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import useEmblaCarousel from 'embla-carousel-react';
+import styled from 'styled-components';
+import { useTheme } from '../hooks/theme';
+import { BaseButton, BASE_BORDER_RADIUS, ElementThemeProps } from '../themes';
+import { Theme } from '@material-ui/core';
+
+const Embla = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
+
+const Viewport = styled.div`
+  overflow: hidden;
+  width: 100%;
+`;
+
+const Container = styled.div`
+  display: flex;
+  will-change: transform;
+`;
+
+const Slide = styled.div`
+  flex: 0 0 auto;
+  width: 80%;
+  position: relative;
+  padding-left: 1rem;
+  counter-increment: embla;
+`;
+
+const SlideInner = styled.div<ElementThemeProps>`
+  position: relative;
+  border-radius: 0.5rem;
+  min-height: 200px;
+  padding-bottom: 46%;
+  font-size: 5rem;
+  box-shadow: ${({ theme }) => theme.BASE_BOX_SHADOW};
+
+  &::before {
+    color: white;
+    font-weight: 300;
+    line-height: 1;
+    content: counter(embla);
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+`;
+
+const Dots = styled.div`
+  display: flex;
+  list-style: none;
+  padding-left: 0;
+  justify-content: center;
+  margin-block-start: 2rem;
+`;
+
+interface DotButtonProps {
+  active: boolean;
+  onClick: () => void;
+  theme: Theme;
+}
+
+const Dot = styled(BaseButton)<DotButtonProps>`
+  position: relative;
+  margin-right: 0.75rem;
+  margin-left: 0.75rem;
+  border: 0;
+  display: flex;
+  align-items: center;
+  border-radius: 50%;
+
+  box-shadow: ${({ active, theme }) => active && theme.INSET_BASE_BOX_SHADOW};
+`;
+
+const DotButton = ({ selected, onClick }) => {
+  const theme = useTheme();
+  return <Dot active={selected} onClick={onClick} theme={theme} />;
+};
+
+const EmblaCarouselComponent = ({ children }) => {
+  const theme = useTheme();
+  const [viewport, embla] = useEmblaCarousel({ loop: false });
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [scrollSnaps, setScrollSnaps] = useState([]);
+
+  const scrollTo = useCallback((index) => embla.scrollTo(index), [embla]);
+
+  useEffect(() => {
+    const onSelect = () => {
+      setSelectedIndex(embla.selectedScrollSnap());
+    };
+    if (embla) {
+      setScrollSnaps(embla.scrollSnapList());
+      embla.on('select', onSelect);
+      onSelect();
+    }
+  }, [embla]);
+
+  return (
+    <Embla>
+      <Viewport ref={viewport}>
+        <Container>
+          {children.map((Child, index) => (
+            <Slide key={index}>
+              <SlideInner theme={theme}>{Child}</SlideInner>
+            </Slide>
+          ))}
+        </Container>
+      </Viewport>
+      <Dots>
+        {scrollSnaps.map((snap, index) => (
+          <DotButton
+            selected={index === selectedIndex}
+            onClick={() => scrollTo(index)}
+            key={index}
+          />
+        ))}
+      </Dots>
+    </Embla>
+  );
+};
+
+export default EmblaCarouselComponent;
--- apps/ui/components/HomePage/HowItWorksSection.tsx
+++ apps/ui/components/HomePage/HowItWorksSection.tsx
@@ -13,6 +13,7 @@ import {
   ElementThemeProps,
   Theme,
 } from '../../themes';
+import EmblaCarouselComponent from '../EmblaCarouselComponent';
 import Title from '../Title';
 import { useDarkMode } from '../../hooks/useDarkMode';
 
@@ -27,164 +28,17 @@ const Root = styled.section`
 `;
 
 const Content = styled.div`
+  width: 100%;
   margin-block-start: 3rem;
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
-const Wrapper = styled.div<ElementThemeProps>`
-  display: flex;
-  border-radius: ${BASE_BORDER_RADIUS};
-  box-shadow: ${({ theme }) => theme.INSET_BASE_BOX_SHADOW};
-  margin-block-end: 3rem;
-  padding: 2rem 2rem 0 2rem;
-
-  @media (max-width: 900px) {
-    flex-direction: column-reverse;
-    padding: 2rem;
-  }
-`;
-
-const NavButton = styled(BaseButton)`
-  max-width: 200px;
-  font-size: 24px;
-  margin-block-end: 3rem;
-`;
-
-const Authors = styled.div`
-  display: flex;
-  align-items: flex-start;
-  flex-direction: column;
-`;
-
-const Instruction = styled.div`
-  width: 500px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  margin-block-end: 1rem;
-
-  @media (max-width: 900px) {
-    width: 250px;
-    margin-block-end: 0;
-  }
-`;
-
-const Header = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-interface InstructionStepProps {
-  active: boolean;
-  theme: Theme;
-}
-
-const InstructionStep = styled(BaseButton)<InstructionStepProps>`
-  height: 50px;
-  width: 50px;
-  border-radius: 50%;
-  box-shadow: ${({ active, theme }) =>
-    active ? theme.INSET_BASE_BOX_SHADOW : theme.BASE_BOX_SHADOW};
-  padding: 0;
-
-  @media (max-width: 900px) {
-    height: 40px;
-    width: 40px;
-    padding: 0;
-  }
-`;
-
-const InstructionHeader = styled.h5`
-  display: inline-block;
-  color: ${POP};
-  font-family: ${FONT_SERIF_BOLD};
-  font-size: 18px;
-  margin: 1rem;
-
-  @media (max-width: 900px) {
-    font-size: 14px;
-  }
-`;
-
-const InstructionDescription = styled.span<ElementThemeProps>`
-  display: inline-block;
-  margin: 0 1rem 1rem calc(50px + 1rem);
-  color: ${({ theme }) => theme.MAIN_TEXT_COLOR};
-  font-family: ${FONT_SERIF_REGULAR};
-  ${FadeInBaseAnimation};
-
-  @media (max-width: 900px) {
-    margin: 1rem 0 1rem 1rem;
-  }
-`;
-
-interface InstructionCompProps {
-  index: number;
-  onClick: (number) => void;
-  currentStep: number;
-  title: string;
-  description: string[];
-}
-
-const Collectors = styled(Authors)`
-  margin-inline-start: 2rem;
-
-  @media (max-width: 900px) {
-    margin-inline-start: 0;
-  }
-`;
-
-const CollectorsWrapper = styled(Wrapper)<ElementThemeProps>`
-  flex-direction: row-reverse;
-
-  @media (max-width: 900px) {
-    flex-direction: column;
-    margin-block-end: 0;
-  }
-`;
-
-const InstructionComp = ({
-  currentStep,
-  index,
-  onClick,
-  title,
-  description,
-}: InstructionCompProps) => {
-  const theme = useTheme();
-  return (
-    <Instruction>
-      <Header>
-        <InstructionStep
-          active={currentStep === index}
-          disabled={currentStep === index}
-          onClick={() => onClick(index)}
-          theme={theme}
-        >
-          {index}
-        </InstructionStep>
-        <InstructionHeader>{title}</InstructionHeader>
-      </Header>
-      {currentStep === index &&
-        description.map((text, idx) => (
-          <InstructionDescription key={idx} theme={theme}>
-            {text}
-          </InstructionDescription>
-        ))}
-    </Instruction>
-  );
-};
-
 const HowItWorksSection = () => {
   const isDarkMode = useDarkMode();
   const theme = useTheme();
-  const [authorInstructionsStep, setAuthorInstructionsStep] = useState<
-    1 | 2 | 3 | 4
-  >(1);
-  const [collectorInstructionsStep, setCollectorInstructionsStep] = useState<
-    1 | 2 | 3 | 4
-  >(1);
+
   return (
     <Root id="howitworks">
       <Title color={POP}>How does it work?</Title>
@@ -201,107 +55,11 @@ const HowItWorksSection = () => {
         </Title>
       </StyledLink>
       <Content>
-        <NavButton disabled>Creators</NavButton>
-        <Wrapper theme={theme}>
-          <Authors>
-            <InstructionComp
-              index={1}
-              currentStep={authorInstructionsStep}
-              onClick={() => setAuthorInstructionsStep(1)}
-              title="Create Your Project"
-              // you don't have a wallet yet?
-              // don't have MATIC yet?
-              description={[
-                `After connecting your wallet, go to the Create tab. Set the title, the text and the amount of NFTs in your first edition (Genesis Edition) – we guide you through the process step by step. You will find your project under the "Bookshelf“ tab later.`,
-              ]}
-            />
-            <InstructionComp
-              index={2}
-              currentStep={authorInstructionsStep}
-              onClick={() => setAuthorInstructionsStep(2)}
-              title="Configure"
-              description={[
-                `Set the price for your NFTs. The first edition of all collections are called Genesis Edition. Only your Genesis Edition will be sold in a dutch auction that can take up to 24 hours, means: you set the starting price and then it goes down continually, until the auction is over.`,
-                `Once it sells out, you can unlock the next edition. In all following editions you can set a fixed minting price.`,
-                `Next, set a cover image. This is what will be shown on marketplaces such as OpenSea. Specify a genre and write a blurb, to give potential collectors insight to your art before minting. Confirm and voilà, your project is created!`,
-              ]}
-            />
-            <InstructionComp
-              index={3}
-              currentStep={authorInstructionsStep}
-              onClick={() => setAuthorInstructionsStep(3)}
-              title="Contributors"
-              description={[
-                `Optional: You can add up to 3 contributors. Maybe you want to share your income with an editor, a cover artist or a translator. Enter their wallet addresses, roles and shares. After an edition sells out, the revenue will be split, for example like this: editor - 15%, cover-artist - 5%, Moonpage project - 15%, YOU – 65%.`,
-              ]}
-            />
-            <InstructionComp
-              index={4}
-              currentStep={authorInstructionsStep}
-              onClick={() => setAuthorInstructionsStep(4)}
-              title="Start Selling!"
-              description={[
-                `After finishing the creation flow, you are forwarded to your project's page. Scroll down and click on "Start Auctions".`,
-                `This transaction transfers 1 NFT of your project to Moonpage and lets you claim some for yourself.`,
-                `Congratulations! You have published your text to the Blockchain. Seriously... THIS IS BIG. Go tell everyone!`,
-              ]}
-            />
-          </Authors>
-          {/* <VideoWrapper>
-            <Video
-              url={'author_step_2.mov'}
-              controls={false}
-              loop
-              muted
-              playing
-              playsinline
-            />
-          </VideoWrapper> */}
-        </Wrapper>
-        <NavButton disabled>Collectors</NavButton>
-        <CollectorsWrapper theme={theme}>
-          <Collectors>
-            <InstructionComp
-              index={1}
-              currentStep={collectorInstructionsStep}
-              onClick={() => setCollectorInstructionsStep(1)}
-              title="Browse Projects"
-              description={[
-                `After connecting your wallet, click on the "Projects" tab. You can search for all kinds of texts here. Top projects are listed on the homepage right above this section.`,
-              ]}
-            />
-            <InstructionComp
-              index={2}
-              currentStep={collectorInstructionsStep}
-              onClick={() => setCollectorInstructionsStep(2)}
-              title="Mint"
-              description={[
-                `The first edition of all collections are called Genesis Edition. Only the Genesis Edition NFTs are sold in a dutch auction. On a project's page click on the "Get Current Price" Button and mint, if it’s not a Genesis Edition, you can click on "mint" right away.`,
-                `Confirm the transaction, wait for a moment and tadaaa! You own your first text NFT!`,
-              ]}
-            />
-            <InstructionComp
-              index={3}
-              currentStep={collectorInstructionsStep}
-              onClick={() => setCollectorInstructionsStep(3)}
-              title="Bookshelf"
-              description={[
-                `As NFT owner, you see a little "READ" button on the cover of the matching project's page. Click on it to read. Or you can go to "Bookshelf", where you find a list of the NFTs you own.`,
-                `Further features for NFT owners will be unlocked in the future, so hold on tight!`,
-              ]}
-            />
-          </Collectors>
-          {/* <VideoWrapper>
-            <Video
-              url={'author_step_2.mov'}
-              controls={false}
-              loop
-              muted
-              playing
-              playsinline
-            />
-          </VideoWrapper> */}
-        </CollectorsWrapper>
+        <EmblaCarouselComponent>
+          <div>Slide 1</div>
+          <div>Slide 2</div>
+          <div>Slide 3</div>
+        </EmblaCarouselComponent>
       </Content>
     </Root>
   );
