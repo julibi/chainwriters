b1f3c7d3b0503624ce9f56c1e72566abf403cec1
juli <juli@polychainmonsters.com>
2022-12-04 03:08:38 +0100

    live debugging yeh

--- apps/ui/components/ProjectItem.tsx
+++ apps/ui/components/ProjectItem.tsx
@@ -120,8 +120,10 @@ const ProjectItem = ({
   );
 
   const getImageUrl = useCallback(async () => {
+    console.log({ id, imgIpfsHash, title });
     if (id && imgIpfsHash?.length) {
       const imgUrl = await getCoverImageUrl(id, imgIpfsHash);
+      console.log({ imgUrl });
       setCoverImgLink(imgUrl);
     }
   }, [id, imgIpfsHash]);
